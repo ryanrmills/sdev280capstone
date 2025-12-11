47856b7e3d3b2639e780fccf34f1211b3ce9a28b
ryanrmills <ryanroymills@gmail.com>
2025-12-11 01:01:43 +0000

    added db.php

---
+++ db.php
@@ -0,0 +1,8 @@
+<?php
+
+    define("DB_HOST", "ryanmillsdev-mariadb.clkackgyodvn.us-west-2.rds.amazonaws.com");
+    define("DB_USER", "mariadb_admin");
+    define("DB_PASS", "Duke!32514");
+    define("DB_NAME", "sdev280capstone");
+
+?>
